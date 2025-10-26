a2d974ba1e11c7a52a4ee3b6721fe53c732d0f31
Vivek Singh <viveksingh9301@gmail.com>
2025-10-26 22:34:52 +0530

    Find the second largest number from a string of digits

---
+++ 2-Second Largest Number/second-largest.ts
@@ -0,0 +1,23 @@
+/**
+ * Finds the second-largest number from the string containing the digits.
+ * @param s string containing the digits
+ * @returns second-largest number if present, -1 otherwise
+ */
+function secondHighest(s: string): number {
+  const onlyNumbersString = s.replace(/[a-zA-Z]/g, "");
+  const numberList = Array.from(new Set(onlyNumbersString.split("").map(Number)));
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  for (const num of numberList) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && secondLargest > largest) {
+      secondLargest = num;
+    }
+  }
+  if (secondLargest > -Infinity) {
+    return secondLargest;
+  }
+  return -1;
+}
