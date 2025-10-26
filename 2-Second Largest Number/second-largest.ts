807bd307125eb45964ccbe4d1b8daf4f844a4c4d
Vivek Singh <viveksingh9301@gmail.com>
2025-10-26 22:36:22 +0530

    Find the second-largest number using sorting (alternative)

--- 2-Second Largest Number/second-largest.ts
+++ 2-Second Largest Number/second-largest.ts
@@ -21,3 +21,25 @@ function secondHighest(s: string): number {
   }
   return -1;
 }
+
+/**
+ * Finds the second-largest number using sort, from the string containing the digits.
+ * @param s string containing the digits
+ * @returns second-largest number if present, -1 otherwise
+ */
+function secondHighestUsingSort(s: string): number {
+  const onlyNumbersString = s.replace(/[a-zA-Z]/g, "");
+  const numberList = Array.from(new Set(onlyNumbersString.split("").map(Number))).sort();
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  for (const num of numberList) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    }
+  }
+  if (secondLargest > -Infinity) {
+    return secondLargest;
+  }
+  return -1;
+}
