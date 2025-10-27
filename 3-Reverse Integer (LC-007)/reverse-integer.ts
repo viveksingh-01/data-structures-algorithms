ef95439b286d6b7b99ded29b13c2d007fab441fa
Vivek Singh <viveksingh9301@gmail.com>
2025-10-27 22:37:23 +0530

    Add overflow check for reversed integer function

--- 3-Reverse Integer (LC-007)/reverse-integer.ts
+++ 3-Reverse Integer (LC-007)/reverse-integer.ts
@@ -1,7 +1,7 @@
 /**
  * Reverses the integer
  * @param x integer to be reversed, a negative number has to be handled accordingly
- * @returns the reversed integer
+ * @returns the reversed integer; 0 in case the reversed number falls out of the range: -2^31 to 2^31 - 1
  */
 function reverse(x: number): number {
   let num = Math.abs(x);
@@ -11,8 +11,9 @@ function reverse(x: number): number {
     rev = rev * 10 + rem;
     num = Math.floor(num / 10);
   }
-  if (x < 0) {
-    return -rev;
+  const limit = Math.pow(2, 31);
+  if (rev < -limit || rev >= limit) {
+    return 0;
   }
-  return rev;
+  return x < 0 ? -rev : rev;
 }
